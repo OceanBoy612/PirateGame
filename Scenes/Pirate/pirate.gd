78e4585afd92a5ed28b3971a99650e45c406b73b
Alexander Morsey <tempthing0@gmail.com>
2023-01-17 00:17:40 -0800

    Working on Pirate AI

--- Scenes/Pirate/pirate.gd
+++ Scenes/Pirate/pirate.gd
@@ -1,19 +1,41 @@
 extends CharacterBody2D
 
-# move mostly striaght avoiding land
+@export var speed = 60
 
-# move toward player
+enum states {PATROL, CHASE, ATTACK}
+var state = states.PATROL
+# Target for chase mode.
+var target = null
 
-# When in range broadside + fire
-
+func _ready():
+	$DetectRadius.body_entered.connect(_on_DetectRadius_area_entered)
+	$DetectRadius.body_exited.connect(_on_DetectRadius_area_exited)
 
 func _physics_process(delta):
-
+	match state:
+		states.PATROL:
+			rotate(0.01)
+		states.CHASE:
+			pass
+#			print(angle_thing)
+#			print(position.angle_to_point(target.position))
+#			if angle_thing > 0:
+#				rotate(0.01)
+#			else:
+#				rotate(-0.01)
+		states.ATTACK:
+			pass
+	
+	velocity = Vector2(0, speed).rotated(rotation)
 	move_and_slide()
 
+func _on_DetectRadius_area_entered(body):
+	print("Enter")
 
-func approach():
-	pass
+func _on_DetectRadius_area_exited(body):
+	print("Exit")
 
-func boradside():
-	pass
+
+#func _on_AttackRadius_body_exited(body):
+#	state = states.CHASE
+
