6f3d9648b64acb3cb7bc92279b3052b5dd68c3ef
Alexander Morsey <tempthing0@gmail.com>
2023-01-24 13:37:44 -0800

    Who knows

---
+++ Ocean.gd
@@ -0,0 +1,13 @@
+#@tool
+extends Sprite2D
+
+
+
+func _on_item_rect_changed():
+	pass
+#	material.set_shader_parameter("aspect_ratio", scale.y / scale.x)
+#	material.set_shader_parameter("tiled_factor", Vector2(scale.x, scale.y))
+
+
+
+
